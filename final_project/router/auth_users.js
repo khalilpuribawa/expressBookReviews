1dbd987be7b997716743863bf2f979f339c5b965
Khalil Puribawa <khalil.puribawa2@gmail.com>
2026-02-25 02:35:35 -0500

    final project

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -9,9 +9,34 @@ const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+// Fungsi pembantu untuk cek login
+const authenticatedUser = (username, password) => {
+    let validusers = users.filter((user) => {
+      return (user.username === username && user.password === password);
+    });
+    return validusers.length > 0;
+  };
+  
+  // Rute Login
+  regd_users.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+      return res.status(404).json({ message: "Error logging in" });
+    }
+  
+    if (authenticatedUser(username, password)) {
+      // Generate JWT Access Token
+      let accessToken = jwt.sign({ data: password }, 'access', { expiresIn: 60 * 60 });
+  
+      // Simpan token di session
+      req.session.authorization = { accessToken, username };
+      return res.status(200).send("User successfully logged in");
+    } else {
+      return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
+  });
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
@@ -19,11 +44,38 @@ regd_users.post("/login", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const isbn = req.params.isbn;
+    const review = req.query.review; // Mengambil ulasan dari request query
+    const username = req.session.authorization.username; // Mengambil user dari session
+  
+    if (books[isbn]) {
+      let book = books[isbn];
+      // Tambahkan atau perbarui ulasan berdasarkan username
+      book.reviews[username] = review;
+      return res.status(200).send(`The review for the book with ISBN ${isbn} has been added/updated.`);
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  });
+
+  regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+  
+    if (books[isbn]) {
+      let book = books[isbn];
+      // Hapus properti ulasan milik user yang sedang login
+      if (book.reviews[username]) {
+        delete book.reviews[username];
+        return res.status(200).send(`Reviews for the ISBN ${isbn} posted by the user ${username} deleted.`);
+      } else {
+        return res.status(404).send("Review not found for this user.");
+      }
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,40 +4,102 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+// Fungsi pembantu untuk mengecek apakah user sudah terdaftar
+const isValid = (username) => {
+  let userswithsamename = users.filter((user) => {
+    return user.username === username;
+  });
+  return userswithsamename.length > 0;
+};
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Cek apakah username dan password disediakan
+  if (username && password) {
+    // Cek apakah username sudah ada
+    if (!isValid(username)) {
+      users.push({ "username": username, "password": password });
+      return res.status(200).json({ message: "User successfully registered. Now you can login" });
+    } else {
+      return res.status(404).json({ message: "User already exists!" });
+    }
+  }
+  return res.status(404).json({ message: "Unable to register user (Username/Password missing)." });
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+// Lokasi: router/general.js
+// Lokasi: router/general.js
+public_users.get('/', async function (req, res) {
+    try {
+      // Simulasi pengambilan data asinkron
+      const response = await axios.get("http://localhost:5000/"); // Mengarah ke rute internal atau endpoint data
+      res.status(200).json(books);
+    } catch (error) {
+      res.status(500).json({ message: "Error fetching book list" });
+    }
+  });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+// Lokasi: router/general.js
+public_users.get('/isbn/:isbn', function (req, res) {
+    const getBook = new Promise((resolve, reject) => {
+      const isbn = req.params.isbn;
+      if (books[isbn]) {
+        resolve(books[isbn]);
+      } else {
+        reject("Book not found");
+      }
+    });
   
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    getBook
+      .then((book) => res.status(200).json(book))
+      .catch((err) => res.status(404).json({ message: err }));
+  });
+  
+// Lokasi: router/general.js
+public_users.get('/author/:author', async function (req, res) {
+    const author = req.params.author;
+    try {
+      const getBooksByAuthor = await new Promise((resolve) => {
+        let filteredBooks = Object.values(books).filter(b => b.author === author);
+        resolve(filteredBooks);
+      });
+      res.status(200).json(getBooksByAuthor);
+    } catch (error) {
+      res.status(404).json({ message: "Author not found" });
+    }
+  });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
+// Lokasi: router/general.js
+public_users.get('/title/:title', function (req, res) {
+    const getByTitle = new Promise((resolve, reject) => {
+      const title = req.params.title;
+      let filteredBooks = Object.values(books).filter(b => b.title === title);
+      
+      if (filteredBooks.length > 0) {
+        resolve(filteredBooks);
+      } else {
+        reject("No books found with this title");
+      }
+    });
+  
+    getByTitle
+      .then((data) => res.status(200).json(data))
+      .catch((err) => res.status(404).json({ message: err }));
+  });
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if (book) {
+    // Mengirimkan hanya objek reviews dari buku tersebut
+    res.send(JSON.stringify(book.reviews, null, 4));
+  } else {
+    res.status(404).json({ message: "Book reviews not found" });
+  }
 });
 
 module.exports.general = public_users;
